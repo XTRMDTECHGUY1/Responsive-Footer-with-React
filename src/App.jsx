7bb4963977aeb30cf36432851944c79596edc5de
Xtrm tech <88555638+XTRMDTECHGUY1@users.noreply.github.com>
2024-05-15 10:10:26 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Body from './Body';
+import Footer from './Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+<div className='grid container'>
+<Body />
+<Footer />
+
+</div>
+     
+    
+  )
+}
+
+export default App
---
+++ src/Body.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import man from './assets/New folder/man.png';
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+
+function body() {
+  return (
+    <div className='div_body'>
+
+
+
+    </div>
+
+
+
+    
+  )
+}
+
+export default body
